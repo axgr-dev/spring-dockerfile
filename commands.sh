6ba58edc748d359c64dd97c82b3251e0e1467aee
Alex Gutjahr <alex@alexgutjahr.com>
2023-11-05 19:46:04 +0100

    Once upon a time..

---
+++ commands.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64 --no-cache -t spring-dockerfile:1.0.0 .
---
+++ src/main/kotlin/dev/axgr/PingController.kt
@@ -0,0 +1,12 @@
+package dev.axgr
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+
+  @GetMapping("/ping")
+  fun ping() = "pong"
+
+}
